329081cbde5e49728f455a3802924b0c48e27dfd
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2024-11-29 14:26:51 +0700

    Tạo files

---
+++ AppLuyenThiA1/src/main/java/ntu/hieutm/appluyenthia1/HelloController.java
@@ -0,0 +1,14 @@
+package ntu.hieutm.appluyenthia1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
